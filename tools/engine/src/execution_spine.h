eb7688a3aacba752e164969d5d228bb0684ae256
Robert Franklin Ard <rard777@gmail.com>
2026-01-27 17:17:46 -0600

    Create execution_spine.h

---
+++ tools/engine/src/execution_spine.h
@@ -0,0 +1,9 @@
+#ifndef SHOOTS_EXECUTION_SPINE_H
+#define SHOOTS_EXECUTION_SPINE_H
+
+#include "engine_internal.h"
+
+void spine_record_intent(shoots_engine_t *engine, intent_t intent);
+void spine_record_result(shoots_engine_t *engine, result_t result);
+
+#endif
